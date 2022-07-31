42e837f9e1099d35bf5e35db2b118071560bf586
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-01 00:51:35 +0300

    add a search bar and style it

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -8,9 +8,10 @@ function Home({ items }) {
     <Navbar />
       <div className="categories">
         <button className="category">All</button>
-        <button className="category">Women Products</button>
-        <button className="category">Men Products</button>
+        <button className="category">Women </button>
+        <button className="category">Men </button>
         <button className="category">Jewelery</button>
+        <input id="search-input" type="text" placeholder="search..."></input>
       </div>
 
       <div className="card-container">
