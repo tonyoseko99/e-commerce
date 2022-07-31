72bfae9c2f318d677bc19b3cb66c17ec0e2ded18
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-01 01:04:40 +0300

    query data from the API through the search bar

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Items from "./Items";
 
 function Home({ items }) {
+  const [searchItems, setSearchItems] = useState(items);
   return (
     <>
-    <Navbar />
+      <Navbar />
       <div className="categories">
         <button className="category">All</button>
         <button className="category">Women </button>
         <button className="category">Men </button>
         <button className="category">Jewelery</button>
-        <input id="search-input" type="text" placeholder="search..."></input>
+        <input
+          id="search-input"
+          type="text"
+          placeholder="search..."
+          onChange={(e) => {
+            let search = items.filter((item) =>
+              item.title.toLowerCase().includes(e.target.value)
+            );
+            console.log("search", search);
+            setSearchItems(search);
+          }}
+        ></input>
       </div>
 
       <div className="card-container">
-        {items.map((item)=>{
-            return (
-                <Items item={item} key={item.id}/>
-            )
+        {items.map((item) => {
+          return <Items item={item} key={item.id} />;
         })}
       </div>
     </>
