b9d3bbef5465ea70e0200f2eab8b3cd6a9e243ee
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-01 21:15:53 +0300

    homepage button resizing

--- my-app/src/components/Items.js
+++ my-app/src/components/Items.js
@@ -2,15 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Item from "./Item";
 
-function Items({item}) {
+function Items({ item }) {
   return (
     <>
       <div className="card" key={item.id}>
         <img src={item.image} alt="title"></img>
         <h2>{item.title}</h2>
         <p>{item.description}</p>
-        <h3>price: ${item.price}</h3>
-        <Link to={`item/${item.id}`} onClick={()=><Item key={item.id} />}>Buy</Link>
+        <div>
+          <h3>price: ${item.price}</h3>
+          <Link to={`item/${item.id}`} onClick={() => <Item key={item.id} />}>
+            Buy
+          </Link>
+        </div>
       </div>
     </>
   );
